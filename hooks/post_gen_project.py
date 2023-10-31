2ff4273fe2f1a6967ab2f2944c33b776cf3354b1
JotaFan <jotaflame@gmail.com>
2023-10-31 12:05:29 +0000

    Try

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,18 +1,18 @@
-import subprocess
-import os
+# import subprocess
+# import os
 
-# setup venv
-new_dir = "{{ cookiecutter.project_name }}"
-os.chdir(new_dir)
+# # setup venv
+# new_dir = "{{ cookiecutter.project_name }}"
+# os.chdir(new_dir)
 
-# Check if virtualenvwrapper is installed
-if subprocess.call(['which', 'virtualenvwrapper']) == 1:
-    subprocess.call(['pip', "install", "virtualenvwrapper"])
-   # virtualenvwrapper is installed, create a new virtual environment
-venv_name = "{{ cookiecutter.project_name }}"
-subprocess.call(['mkvirtualenv', venv_name])
-subprocess.call(['setvirtualenvproject'])
+# # Check if virtualenvwrapper is installed
+# if subprocess.call(['which', 'virtualenvwrapper']) == 1:
+#     subprocess.call(['pip', "install", "virtualenvwrapper"])
+#    # virtualenvwrapper is installed, create a new virtual environment
+# venv_name = "{{ cookiecutter.project_name }}"
+# subprocess.call(['mkvirtualenv', venv_name])
+# subprocess.call(['setvirtualenvproject'])
 
-# Update poetry to the latest version
-subprocess.call(['poetry', 'self', 'update'])
-subprocess.call(['make', 'init-dev'])
+# # Update poetry to the latest version
+# subprocess.call(['poetry', 'self', 'update'])
+# subprocess.call(['make', 'init-dev'])
