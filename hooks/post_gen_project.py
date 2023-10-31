7577f4c4e6115574ed9ea732f3e2b6c68b3fe880
JotaFan <jotaflame@gmail.com>
2023-10-31 00:19:56 +0000

    Update commands post gen and some variables

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,16 @@
 import subprocess
 
 # setup venv
+new_dir = "{{ cookiecutter.project_name }}"
+os.chdir(new_dir)
+
+# Check if virtualenvwrapper is installed
+if subprocess.call(['which', 'virtualenvwrapper']) == 0:
+   # virtualenvwrapper is installed, create a new virtual environment
+   venv_name = "{{ cookiecutter.project_name }}_venv"
+   subprocess.call(['mkvirtualenv', venv_name])
+   subprocess.call(['setvirtualenvproject'])
+
+# Update poetry to the latest version
+subprocess.call(['poetry', 'self', 'update'])
 subprocess.call(['make', 'init-dev'])
