105ca5b80e2d3d731e13f1173b86ca1026deea35
JotaFan <jotaflame@gmail.com>
2023-10-31 00:36:16 +0000

    Add missing import

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,10 +7,11 @@ os.chdir(new_dir)
 
 # Check if virtualenvwrapper is installed
 if subprocess.call(['which', 'virtualenvwrapper']) == 1:
+    subprocess.call(['pip', "install", "virtualenvwrapper"])
    # virtualenvwrapper is installed, create a new virtual environment
-   venv_name = "{{ cookiecutter.project_name }}_venv"
-   subprocess.call(['mkvirtualenv', venv_name])
-   subprocess.call(['setvirtualenvproject'])
+venv_name = "{{ cookiecutter.project_name }}"
+subprocess.call(['mkvirtualenv', venv_name])
+subprocess.call(['setvirtualenvproject'])
 
 # Update poetry to the latest version
 subprocess.call(['poetry', 'self', 'update'])
